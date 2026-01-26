b07ca5e028440e46c651ecc81c8640a0a65699f4
slendchat <slendchat@gmail.com>
2026-01-26 22:23:57 +0200

    feat:logger,log retention

--- PruneFoldersByRootSize.ps1
+++ PruneFoldersByRootSize.ps1
@@ -3,8 +3,36 @@
 [CmdletBinding(SupportsShouldProcess = $true)]
 param(
     [Parameter(Mandatory)][string] $Root,
-    [Parameter(Mandatory)][int64] $MaxSize
+    [Parameter(Mandatory)][int64] $MaxSize,
+    [string] $LogDirMain = "$Root\LOG",
+    [int] $RetentionPeriod = 90
 )
+
+function Write-Log {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Message,
+
+        [ValidateSet("INFO", "WARN", "ERROR", "DEBUG")]
+        [string]$Level = "INFO",
+
+        [string]$LogDir = $LogDirMain
+    )
+
+    $TimeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $DateStamp = Get-Date -Format "yyyy-MM-dd"
+    $LogFile = $LogDir + "\PruneFoldersByRootSize_$DateStamp.log"
+    $LogString = "$TimeStamp [$Level] $Message"
+
+    if (!(Test-Path -PathType Container $LogDir)) {
+        New-Item -ItemType Directory -Path $LogDir | Out-Null
+    }
+
+    $LogString | Out-File -FilePath $LogFile -Append -Encoding utf8
+
+    Write-Verbose $LogString
+}
 
 function Get-dirSizeBytes {
     param (
@@ -12,50 +40,73 @@ function Get-dirSizeBytes {
     )
 
     $DirSize = (Get-ChildItem -LiteralPath $Path -File -Recurse -ErrorAction Stop | Measure-Object -Sum Length).Sum
-    if(-not $DirSize){
-        throw "[ERROR] Failed to calculate Root dir size"
+    if($null -eq $DirSize){
+        Write-Log -Message "Failed to calculate Root dir size: $Root" -Level "ERROR"
+        throw
     }
     [int64]$DirSize
 }
 
-if(-not(Test-Path -LiteralPath $Root)){
-    throw "[ERROR] Path is not valid"
+function Invoke-Log-Retention {
+    param (
+        [string]$LogDir = $LogDirMain,
+        [int]$RetentionPeriod = 90
+    )
+    if (-not(Test-Path $LogDir)){
+        Write-Log -Message "Cannot find log dir to Invoke-Log-Retention" -Level "ERROR"
+    }
+    $Cutoff = (Get-Date).AddDays(-$RetentionPeriod)
+    Get-ChildItem -Path $LogDir -Filter 'PruneFoldersByRootSize_*.log' -File |
+    Where-Object {$_.LastWriteTime -lt $Cutoff} |
+    Remove-Item -Force -ErrorAction SilentlyContinue
 }
 
+############################ SCRIPT STARTS ############################
+Write-Log -Message "=== Script Start Executing ==="
+
+Invoke-Log-Retention -RetentionPeriod 90 -LogDir $LogDirMain
+
+if(-not(Test-Path -LiteralPath $Root)){
+    Write-Log -Message "Path is not valid" -Level "ERROR"
+    throw
+}
 
 $RootDirTotalSize = Get-dirSizeBytes -Path $Root
-Write-Debug "[DEBUG] Root dir size - $($RootDirTotalSize)"
+Write-Log -Message "Root dir size - $([math]::Round(($RootDirTotalSize / 1MB),2)) MB" -Level "DEBUG"
 
-if ($RootDirTotalSize -lt $MaxSize) {
-    Write-Host "[INFO] The root folder smaller the given size" 
+if ($RootDirTotalSize -lt ($MaxSize * 1MB)) {
+    Write-Log -Message "The root folder smaller the given size" -Level "INFO"
+    Write-Log -Message "=== Exiting Script ==="
     return
 }
 
 $SubDirs = Get-ChildItem -LiteralPath $Root -Directory -Force -ErrorAction Stop | Sort-Object LastWriteTimeUtc
 
 foreach ($SubDir in $SubDirs) {
+    # Skip log dir
+    if($SubDir.Name -eq 'LOG') { continue }
     $SubDirSize = Get-dirSizeBytes -Path $SubDir.FullName
-    Write-Debug "[DEBUG] SubDir size - $($SubDirSize)"
+    Write-Debug "[DEBUG] SubDir size - $([math]::Round(($SubDirSize / 1MB),2)) MB"
 
     if($PSCmdlet.ShouldProcess($SubDir.FullName,"DELETE")){
         try {
             Remove-Item -LiteralPath $SubDir.FullName -Recurse -Force -ErrorAction Stop
             $RootDirTotalSize -= $SubDirSize   
-            Write-Debug "[DEBUG] Folder $($SubDir.FullName) was DELETED"
+            Write-Log -Message "Folder $($SubDir.FullName) was DELETED" -Level "DEBUG"
         }
         catch {
-            Write-Warning "[WARNING] Failed to delete folder \n $($_.Exception.Message)"
+            Write-Log -Message "Failed to delete folder \n $($_.Exception.Message)" -Level "WARN"
         }
     } else {
-        Write-Debug "[DEBUG] Folder $($SubDir.FullName) could be DELETED"
+        Write-Log -Message "Folder $($SubDir.FullName) could be DELETED" -Level "DEBUG"
         $RootDirTotalSize -= $SubDirSize
     }
-
-    Write-Debug "[DEBUG] Root dir size after deleting - $($RootDirTotalSize)"
-
-    if ($RootDirTotalSize -lt $MaxSize) {
+    
+    if ($RootDirTotalSize -lt ($MaxSize * 1MB)) {
+        Write-Log -Message "Root dir size after deleting subfolders - $([math]::Round(($RootDirTotalSize / 1MB),2)) MB" -Level "DEBUG"
         break
     }
 }
 
-Write-Host "[INFO] Done"
+
+Write-Log -Message "=== Exiting Script ==="
